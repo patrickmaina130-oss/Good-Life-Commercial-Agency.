f0e7868be9af32a54e4cc200da4c00cb1f0579f4
patrickmaina130-oss <patrickmaina130@gmail.com>
2026-06-25 12:57:32 +0300

    Implement automatic slideshow functionality
    Added a slideshow feature that automatically transitions between slides every 5 seconds.

--- script.js
+++ script.js
@@ -370,3 +370,35 @@ updateNavbar();
 updateActiveNav();
 
 updateBackToTop();
+
+/* ==========================
+SLIDESHOW
+========================== */
+
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+slides.forEach(slide => {
+slide.classList.remove('active');
+});
+
+slides[index].classList.add('active');
+
+}
+
+function nextSlide(){
+
+currentSlide++;
+
+if(currentSlide >= slides.length){
+currentSlide = 0;
+}
+
+showSlide(currentSlide);
+
+}
+
+setInterval(nextSlide, 5000);
