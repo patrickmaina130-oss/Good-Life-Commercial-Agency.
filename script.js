4fc61d0e2cbdfcfd59a50cea4cc4acdaa90897e9
patrickmaina130-oss <patrickmaina130@gmail.com>
2026-06-29 15:02:37 +0300

    Add initial script.js with loading message

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+/* ==========================================
+   GOOD LIFE COMMERCIAL AGENCIES
+   SCRIPT.JS
+   VERSION 1.0
+========================================== */
 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Good Life Commercial Agencies loaded successfully.");
+});
