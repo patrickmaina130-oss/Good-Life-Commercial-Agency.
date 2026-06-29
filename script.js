9131f12623105eac9051ea38d26d8c6b0010e3cb
patrickmaina130-oss <patrickmaina130@gmail.com>
2026-06-29 09:01:51 +0300

    Remove sticky navigation effect from script.js
    Removed sticky navigation effect functionality.

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 
-// Sticky Navigation Effect
-
-window.addEventListener("scroll",()=>{
-
-    const navbar=document.querySelector(".navbar");
-
-    if(window.scrollY>30){
-
-        navbar.classList.add("scrolled");
-
-    }else{
-
-        navbar.classList.remove("scrolled");
-
-    }
-
-});
