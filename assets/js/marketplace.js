59375dc5a9dab894fbee828d51dd9bb1972d3a5a
patrickmaina130-oss <patrickmaina130@gmail.com>
2026-07-01 14:25:07 +0300

    Create marketplace.js

---
+++ assets/js/marketplace.js
@@ -0,0 +1,132 @@
+/*==================================================
+GOOD LIFE COMMERCIAL AGENCIES
+MARKETPLACE DATABASE
+==================================================*/
+
+const marketplaceData = {
+
+tours: [
+
+{
+title: "Maasai Mara Safari",
+location: "Narok, Kenya",
+priceUSD: "299 - 499",
+priceKES: "38,500 - 64,500",
+rating: 4.9,
+badge: "Safari",
+image: "assets/images/tours/maasai-mara.jpg"
+},
+
+{
+title: "Amboseli National Park",
+location: "Kajiado, Kenya",
+priceUSD: "250 - 420",
+priceKES: "32,000 - 54,000",
+rating: 4.8,
+badge: "Wildlife",
+image: "assets/images/tours/amboseli.jpg"
+},
+
+{
+title: "Diani Beach Holiday",
+location: "Kwale, Kenya",
+priceUSD: "350 - 650",
+priceKES: "45,000 - 84,000",
+rating: 4.9,
+badge: "Beach",
+image: "assets/images/tours/diani.jpg"
+}
+
+],
+
+hotels: [
+
+{
+title: "Villa Rosa Kempinski",
+location: "Nairobi",
+priceUSD: "240 - 450",
+priceKES: "31,000 - 58,000",
+rating: 4.9,
+badge: "Luxury",
+image: "assets/images/hotels/kempinski.jpg"
+},
+
+{
+title: "Sarova Whitesands",
+location: "Mombasa",
+priceUSD: "180 - 320",
+priceKES: "23,000 - 41,000",
+rating: 4.8,
+badge: "Beach Resort",
+image: "assets/images/hotels/whitesands.jpg"
+}
+
+],
+
+vehicles: [
+
+{
+title: "Toyota Prado",
+location: "Self Drive",
+priceUSD: "95 - 180",
+priceKES: "12,000 - 23,000",
+rating: 4.9,
+badge: "SUV",
+image: "assets/images/vehicles/prado.jpg"
+},
+
+{
+title: "Mercedes S-Class",
+location: "Chauffeur",
+priceUSD: "180 - 350",
+priceKES: "23,000 - 45,000",
+rating: 5.0,
+badge: "Executive",
+image: "assets/images/vehicles/sclass.jpg"
+}
+
+],
+
+realestate: [
+
+{
+title: "3 Bedroom Apartment",
+location: "Westlands",
+priceUSD: "135,000",
+priceKES: "17.5M",
+rating: 4.9,
+badge: "Apartment",
+image: "assets/images/real-estate/apartment.jpg"
+}
+
+],
+
+corporate: [
+
+{
+title: "Business Travel",
+location: "Corporate",
+priceUSD: "450+",
+priceKES: "58,000+",
+rating: 5.0,
+badge: "Executive",
+image: "assets/images/corporate/business.jpg"
+}
+
+],
+
+community: [
+
+{
+title: "Tree Planting",
+location: "Kenya",
+priceUSD: "FREE",
+priceKES: "Donation",
+rating: 5.0,
+badge: "Environment",
+image: "assets/images/community/tree.jpg"
+}
+
+]
+
+};
